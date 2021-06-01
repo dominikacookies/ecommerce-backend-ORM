fee528df3dc715206bc4bab799bc0e8ee289862b
Dominika <domi_nika@hotmail.co.uk>
2021-06-01 17:11:41 +0100

    gets all tag info

--- server/routes/api/tag-routes.js
+++ server/routes/api/tag-routes.js
@@ -3,9 +3,24 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+// find all tags
+router.get('/', async (req, res) => {
+  try {
+    const allTags = await Tag.findAll({
+      include: [{
+                  model: Product,
+                  as: 'products',
+                  through: ProductTag
+                }]
+    })
+    if (!allTags) {
+      res.status(404).json({ message: "There are no tags at the moment."})
+    }
+    res.status(200).json(allTags)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({error: "Sorry we couldn't get all tag information at this time. Please try again later."})
+  }
 });
 
 router.get('/:id', (req, res) => {
