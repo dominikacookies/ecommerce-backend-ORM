aec15cd2b2e059c3f02810f3d447a0a5ea45ade4
Dominika <domi_nika@hotmail.co.uk>
2021-06-02 17:53:32 +0100

    updates tag

--- server/routes/api/tag-routes.js
+++ server/routes/api/tag-routes.js
@@ -23,21 +23,68 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+// find a single tag by its `id`
+router.get('/:id', async (req, res) => {
+  try {
+    const tag = await Tag.findByPk(req.params.id, { 
+      include: [{
+        model: Product,
+        as: 'products',
+      }]
+    });
+    if (!tag) {
+      res.status(404).json({error: "No tag found with this id"})
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: "Sorry, we couldn't get your tag information at this time"})
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+// create a new tag
+router.post('/', async (req, res) => {
+  try {
+    const newTag = await Tag.create(req.body);
+    res.status(200).json({
+      message: "A new tag has been successfully created",
+      category: newTag,
+    })
+  } catch (error) {
+    res.status(500).json({error: "Sorry, we were unable to create a new tag at this time. Please try again later."})
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+// update a tag's name by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const {tag_name} = req.body
+    await Tag.update( {tag_name}, {
+      where: {
+        id: req.params.id,
+      },
+    })
+    res.status(200).json({message: "successfully updated tag"})
+  } catch (error) {
+    res.status(500).json({error: "Sorry, we couldn't update your tag at this time"})
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+// delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    await Tag.destroy({
+      where: { id : req.params.id}
+    });
+
+    res.status(200).json({
+      message: "The tag has been successfully deleted",
+    })
+  } catch (error) {
+    res.status(500).json({
+      message: "We were unable to delete the tag at this time. Please try again later."
+    }) 
+  }
 });
 
 module.exports = router;
